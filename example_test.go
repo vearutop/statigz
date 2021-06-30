c78f4507de9636246e596b37f02080000bfb8753
Viacheslav Poturaev <vearutop@users.noreply.github.com>
2021-06-30 11:18:45 +0200

    Update dependencies and README (#8)

--- example_test.go
+++ example_test.go
@@ -2,6 +2,7 @@ package statigz_test
 
 import (
 	"embed"
+	"io/fs"
 	"log"
 	"net/http"
 
@@ -15,8 +16,13 @@ import (
 var st embed.FS
 
 func ExampleFileServer() {
+	s, err := fs.Sub(st, "_testdata")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Plug static assets handler to your server or router.
-	err := http.ListenAndServe(":80", statigz.FileServer(st, brotli.AddEncoding))
+	err = http.ListenAndServe(":80", statigz.FileServer(s.(fs.ReadDirFS), brotli.AddEncoding))
 	if err != nil {
 		log.Fatal(err)
 	}
